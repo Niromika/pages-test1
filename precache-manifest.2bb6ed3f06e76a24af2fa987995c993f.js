602ebc082560c5cc2ea4deefb9483f409fc10da6
Nir Adar <niromika88@gmail.com>
2020-07-19 10:03:13 +0300

    Updates

---
+++ precache-manifest.2bb6ed3f06e76a24af2fa987995c993f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1edf2cbb1d2abbf3f9df43371296f3ca",
+    "url": "/pages-test1/index.html"
+  },
+  {
+    "revision": "d1d4201985321ac0375f",
+    "url": "/pages-test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d18c2c20ab700499acb",
+    "url": "/pages-test1/static/js/2.ea0e36c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages-test1/static/js/2.ea0e36c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d4201985321ac0375f",
+    "url": "/pages-test1/static/js/main.5ed131ac.chunk.js"
+  },
+  {
+    "revision": "23bc314dc2e815d28120",
+    "url": "/pages-test1/static/js/runtime-main.d82abd81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages-test1/static/media/logo.5d5d9eef.svg"
+  }
+]);
